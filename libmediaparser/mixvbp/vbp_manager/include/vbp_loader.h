2f343766e4d782b86bc8e027bbb192a179320a54
Christopher R. Palmer <crpalmer@gmail.com>
2015-12-30 05:39:17 -0500

    intel: mixvbp: LP blob compatibility
    Change-Id: Ie0dfdee20e2341217d1eec1d6bf5b157d8889670

--- libmediaparser/mixvbp/vbp_manager/include/vbp_loader.h
+++ libmediaparser/mixvbp/vbp_manager/include/vbp_loader.h
@@ -74,13 +74,19 @@ typedef struct _vbp_codec_data_mp42
     uint8   par_width;
     uint8   par_height;
 
+#ifdef ASUS_ZENFONE2_LP_BLOBS
+    int wtf_did_they_add_here;
+#endif
+
     // bit rate
     int bit_rate;
 
+#ifndef ASUS_ZENFONE2_LP_BLOBS
     // indicate if vol is received
     uint8 got_vol;
     // indicate if vop is received
     uint8 got_vop;
+#endif
 } vbp_codec_data_mp42;
 
 typedef struct _vbp_slice_data_mp42
@@ -212,7 +218,9 @@ typedef struct _vbp_codec_data_h264
     uint8 constraint_set2_flag;
     uint8 constraint_set3_flag;
     uint8 constraint_set4_flag;
+#ifndef ASUS_ZENFONE2_LP_BLOBS
     uint8 constraint_set5_flag;
+#endif
 
     uint8 num_ref_frames;
     uint8 gaps_in_frame_num_value_allowed_flag;
